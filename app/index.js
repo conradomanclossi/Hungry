24ab93a2e8883753a02122d87d065103cd922904
Conrado Manclossi <conrado.macalossi@gmail.com>
2020-04-04 00:45:57 +0200

    Dockerfile

--- app/index.js
+++ app/index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
 
-app.get('/', (req, res) => res.send("Hello, World!!!"))
+app.get('/', (req, res) => res.send("Você é o melhor"))
 
 app.listen(4000, () => console.log("Listening on port 4000!"))
